2be481cdb19bec2e1953ba55aa137b497a4229ab
clintar <clintar@clint.guru>
2014-02-18 13:14:27 -0700

    fix up some declspecs

--- xptMiner/metiscoinMiner.cpp
+++ xptMiner/metiscoinMiner.cpp
@@ -18,11 +18,11 @@ void metiscoin_process(minerMetiscoinBlock_t* block)
 	static unsigned char	 pblank[1];
 	block->nonce = 0;
 	uint32 target = *(uint32*)(block->targetShare+28);
-	#ifdef _WIN32
-	__declspec(align(32)) uint64 hash0[4];
+#ifdef _WIN32
 	__declspec(align(32)) unsigned int metisPartData[36*GROUPED_HASHES];
 	__declspec(align(32)) uint64 hash0[8*GROUPED_HASHES];
 #else
+
 	__attribute__((aligned(32))) unsigned int metisPartData[36*GROUPED_HASHES];
 	__attribute__((aligned(32))) uint64 hash0[8*GROUPED_HASHES];
 #endif
