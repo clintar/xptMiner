59a14dbe3673e2a9c18b096ade856c3ccbe848a7
clintar <clintar@clint.guru>
2014-02-20 02:07:29 -0700

    gotta figure out what happened because of this....

--- xptMiner/sph_metis.h
+++ xptMiner/sph_metis.h
@@ -75,7 +75,7 @@ void sph_metis512_addbits_and_close(
 	void *cc, unsigned ub, unsigned n, void *dst);
 
 // optimized routines
-unsigned int metis4_core_opt(const void *data);
+unsigned int metis4_core_opt(unsigned int* data);
 void metis4_core_opt_interleaved(unsigned int* data1, unsigned int* data2, unsigned int* v1, unsigned int* v2);
 
 void metis4_core_opt_p1(unsigned int* data, unsigned int* pOut);
